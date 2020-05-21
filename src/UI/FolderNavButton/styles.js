c8610f23a0211023c749edc34793238defb701ad
Mateusz Walendzik <mate.walendzik@gmail.com>
2020-05-21 15:06:36 +0200

    Mateuszwalendzik/ch15829/ui navigation buttons (#12)
    * add basic structure, without much styling
    
    * add padding to button component, style navigation folder buttons

---
+++ src/UI/FolderNavButton/styles.js
@@ -0,0 +1,13 @@
+import { makeStyles } from '@material-ui/core/styles';
+
+export default makeStyles((theme) => ({
+  root: {
+    padding: 0,
+  },
+  icon: {
+    fontSize: 18,
+  },
+  disabledIcon: {
+    color: theme.palette.palette.gray2,
+  }
+}));
