4c41c4d1f69d4b68f0105cd0d520b1a0faea8d7a
Guillermo Puente <guillermo.ps09@gmail.com>
2020-05-22 11:50:39 -0400

    Added CSSBaseline to storybook

--- .storybook/config.js
+++ .storybook/config.js
@@ -2,12 +2,14 @@ import React from 'react';
 import { configure, addDecorator } from '@storybook/react';
 import { withKnobs } from '@storybook/addon-knobs/react';
 import { ThemeProvider } from '@material-ui/core/styles';
+import { CssBaseline } from '@material-ui/core';
 import createFleekTheme from '@ui/theme';
 
 const theme = createFleekTheme();
 
 const withProviders = (story) => (
   <ThemeProvider theme={theme}>
+    <CssBaseline />
     {story()}
   </ThemeProvider>
 );
