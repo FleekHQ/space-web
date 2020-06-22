aa3bd3af6e04c6ed8886e378150f0511022797ac
Guillermo Puente <guillermo.ps09@gmail.com>
2020-06-22 10:24:34 -0400

    Fix color

--- src/views/Modal/modals/Sharing/styles.js
+++ src/views/Modal/modals/Sharing/styles.js
@@ -1,7 +1,7 @@
 import { makeStyles } from '@material-ui/core/styles';
 
-export default makeStyles({
+export default makeStyles((theme) => ({
   root: {
-    backgroundColor: 'white',
+    backgroundColor: theme.palette.palette.white,
   },
-});
+}));
