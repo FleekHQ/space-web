66265cc94b7e15d2b69f8bdb5350d1e6bd93ec21
Guillermo Puente <guillermo.ps09@gmail.com>
2020-08-13 17:23:45 -0400

    Added Prompt component

---
+++ src/shared/components/Modal/Prompt/Prompt.js
@@ -0,0 +1,118 @@
+/* eslint-disable */
+import React, { useState } from 'react';
+import PropTypes from 'prop-types';
+import BaseModal from '@ui/BaseModal';
+import Typography from '@ui/Typography';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faTimes } from '@fortawesome/pro-light-svg-icons/faTimes';
+import ButtonBase from '@material-ui/core/ButtonBase';
+import TextField from '@ui/TextField';
+import Button from '@material-ui/core/Button';
+
+import useStyles from './styles';
+
+const Prompt = (props) => {
+  const {
+    i18n,
+    title,
+    onSubmit,
+    validate,
+    closeModal,
+    validateOnChange,
+    validateOnSubmit,
+  } = props;
+
+  const [error, setError] = useState();
+  const [value, setValue] = useState('');
+
+  const classes = useStyles();
+
+  const onChange = (e) => {
+    if (validateOnChange) {
+      setError(validate(e.target.value));
+    }
+
+    setValue(e.target.value);
+  };
+
+  const onSubmitForm = (e) => {
+    e.preventDefault();
+
+    if (validateOnSubmit) {
+      const validationError = validate(value);
+
+      setError(validationError);
+      if (!validationError) onSubmit(value, validationError, closeModal);
+    } else {
+      onSubmit(value, error, closeModal);
+    }
+  };
+
+  return (
+    <BaseModal
+      paperProps={{
+        className: classes.root,
+      }}
+    >
+      <div className={classes.header}>
+        <Typography variant="h6">
+          {title}
+        </Typography>
+        <ButtonBase onClick={closeModal}>
+          <FontAwesomeIcon
+            icon={faTimes}
+            className={classes.icon}
+          />
+        </ButtonBase>
+      </div>
+      <div>
+        <form onSubmit={onSubmitForm}>
+          <TextField
+            fullWidth
+            label={i18n.label}
+            value={value}
+            onChange={onChange}
+            error={!!error}
+            className={classes.textField}
+          />
+          {error && (
+            <Typography variant="body2" color="secondary" className={classes.errorMessage}>
+              {error}
+            </Typography>
+          )}
+          <div className={classes.buttonContainer}>
+            <Button
+              onClick={closeModal}
+              color="secondary"
+              variant="outlined"
+            >
+              {i18n.cancel}
+            </Button>
+            <Button
+              type="submit"
+              variant="contained"
+            >
+              {i18n.submit}
+            </Button>
+          </div>
+        </form>
+      </div>
+    </BaseModal>
+  );
+};
+
+Prompt.propTypes = {
+  title: PropTypes.string,
+  validate: PropTypes.func,
+  onSubmit: PropTypes.func,
+  closeModal: PropTypes.func,
+  validateOnChange: PropTypes.bool,
+  validateOnSubmit: PropTypes.bool,
+  i18n: PropTypes.shape({
+    label: PropTypes.string,
+    cancel: PropTypes.string,
+    submit: PropTypes.string,
+  }),
+};
+
+export default Prompt;
