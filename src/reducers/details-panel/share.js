43f04ae41a2faa1fa05b39d75d0b4df7b465e2f3
Giancarlo Otarola <g.otarolarobledo@gmail.com>
2020-08-26 15:35:42 -0400

    shareFilesByPublicKey integration

--- src/reducers/details-panel/share.js
+++ src/reducers/details-panel/share.js
@@ -4,6 +4,11 @@ const DEFAULT_STATE = {
     success: false,
     loading: false,
   },
+  shareFileByPublicKey: {
+    error: null,
+    success: false,
+    loading: false,
+  },
 };
 
 export const SHARE_TYPES = {
@@ -11,6 +16,9 @@ export const SHARE_TYPES = {
   ON_GENERATE_LINK_RESET: 'ON_GENERATE_LINK_RESET',
   ON_GENERATE_LINK_ERROR: 'ON_GENERATE_LINK_ERROR',
   ON_GENERATE_LINK_SUCCESS: 'ON_GENERATE_LINK_SUCCESS',
+  ON_SHARE_FILE_BY_PUBLIC_KEY: 'ON_SHARE_FILE_BY_PUBLIC_KEY',
+  ON_SHARE_FILE_BY_PUBLIC_KEY_ERROR: 'ON_SHARE_FILE_BY_PUBLIC_KEY_ERROR',
+  ON_SHARE_FILE_BY_PUBLIC_KEY_SUCCESS: 'ON_SHARE_FILE_BY_PUBLIC_KEY_SUCCESS',
 };
 
 export default (state = DEFAULT_STATE, action) => {
@@ -51,6 +59,36 @@ export default (state = DEFAULT_STATE, action) => {
         ...state,
         generateLink: {
           ...DEFAULT_STATE.generateLink,
+        },
+      };
+    }
+    case SHARE_TYPES.ON_SHARE_FILE_BY_PUBLIC_KEY: {
+      return {
+        ...state,
+        shareFileByPublicKey: {
+          error: null,
+          loading: true,
+          success: false,
+        },
+      };
+    }
+    case SHARE_TYPES.ON_SHARE_FILE_BY_PUBLIC_KEY_ERROR: {
+      return {
+        ...state,
+        shareFileByPublicKey: {
+          loading: false,
+          success: false,
+          error: action.error,
+        },
+      };
+    }
+    case SHARE_TYPES.ON_SHARE_FILE_BY_PUBLIC_KEY_SUCCESS: {
+      return {
+        ...state,
+        shareFileByPublicKey: {
+          error: null,
+          succes: true,
+          loading: false,
         },
       };
     }
