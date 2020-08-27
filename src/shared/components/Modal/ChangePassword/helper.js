3bc2fca97135202448e5af9008705437b2be6eb4
Giancarlo Otarola <g.otarolarobledo@gmail.com>
2020-08-27 09:56:33 -0400

    clenaup

--- src/shared/components/Modal/ChangePassword/helper.js
+++ src/shared/components/Modal/ChangePassword/helper.js
@@ -1,4 +1,4 @@
-// at least one upper case, 1 lower case, 1 digit, 1 special char and min 9 chars length
+// at least one upper case, 1 lower case, 1 digit, 1 special char and min 8 chars length
 const PASSWORD_REGEX = new RegExp(/^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,}$/);
 
 /**
