fb2c6e46730b6e333763c28ba42d590fbf11f672
Giancarlo Otarola <g.otarolarobledo@gmail.com>
2020-09-04 16:58:35 -0400

    add space services url

--- public/electron/clients/api/index.js
+++ public/electron/clients/api/index.js
@@ -2,7 +2,7 @@ const ApiClient = require('./client');
 
 const apiClient = new ApiClient({
   timeout: 5000,
-  baseURL: process.env.API_URL || 'https://xaoa4ax6v9.execute-api.us-west-2.amazonaws.com/dev',
+  baseURL: process.env.SPACE_SERVICES_URL || 'https://xaoa4ax6v9.execute-api.us-west-2.amazonaws.com/dev',
 });
 
 module.exports = apiClient;
