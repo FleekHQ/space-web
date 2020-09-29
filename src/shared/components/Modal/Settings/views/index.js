d609222edf4282c058c0e43f43079b84debc8ad9
Giancarlo Otarola <g.otarolarobledo@gmail.com>
2020-09-29 18:22:45 -0300

    product key section, product key buy card, uncomment usage and remove switch

--- src/shared/components/Modal/Settings/views/index.js
+++ src/shared/components/Modal/Settings/views/index.js
@@ -1,3 +1,4 @@
 export { default as Account } from './Account';
 export { default as Security } from './Security';
 export { default as Usage } from './Usage';
+export { default as ProductKey } from './ProductKey';
