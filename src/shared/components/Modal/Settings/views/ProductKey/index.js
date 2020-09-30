b847fed33a448126623bfff7e2c3c55f81ec63da
Mateusz Walendzik <mate.walendzik@gmail.com>
2020-09-30 15:49:10 +0200

    add component to enter product key (#252)

--- src/shared/components/Modal/Settings/views/ProductKey/index.js
+++ src/shared/components/Modal/Settings/views/ProductKey/index.js
@@ -1,40 +1,82 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { shell } from 'electron';
 import { useTranslation } from 'react-i18next';
 
 import Box from '@material-ui/core/Box';
 import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
-
+import TextField from '@material-ui/core/TextField';
 import MessageBox from '@ui/MessageBox';
+
+import useStyles from './styles';
 
 const ProductKey = () => {
   const { t } = useTranslation();
+  const [key, setKey] = useState('');
+  const classes = useStyles();
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+    if (key) {
+      // eslint-disable-next-line no-console
+      console.log(key);
+    }
+  };
 
   return (
-    <Box>
-      <MessageBox
-        type="shopping"
-        bgColor="secondary"
-        title={(
-          <Typography>
-            <Box component="span" fontWeight={600}>
-              {t('modals.settings.productKey.messageBox.title')}
-            </Box>
+    <>
+      <div className={classes.colorBorder}>
+        <form className={classes.contentWrapper} onSubmit={onSubmit}>
+          <img
+            alt="Space logo mark"
+            // eslint-disable-next-line max-len
+            src="https://storage.googleapis.com/terminal-assets/images/space-logo-mark.svg"
+            className={classes.logo}
+          />
+          <Typography className={classes.title}>
+            {t('modals.settings.productKey.form.title')}
           </Typography>
-        )}
-      >
-        <Box mb="11px">
-          <Typography>
-            {t('modals.settings.productKey.messageBox.description')}
+          <Typography className={classes.message}>
+            {t('modals.settings.productKey.form.message')}
           </Typography>
-        </Box>
-        {/* TODO: change to real space billing url */}
-        <Button variant="contained" onClick={() => shell.openExternal('https://space.storage')}>
-          {t('modals.settings.productKey.messageBox.buy')}
-        </Button>
-      </MessageBox>
-    </Box>
+          <Box m="16px 0 8px">
+            <TextField
+              label={t('modals.settings.productKey.form.inputLabel')}
+              variant="outlined"
+              value={key}
+              onChange={(event) => setKey(event.target.value)}
+              fullWidth
+            />
+          </Box>
+          <Button disabled={!key} variant="contained">
+            {t('modals.settings.productKey.form.submit')}
+          </Button>
+        </form>
+      </div>
+      <Box>
+        <MessageBox
+          type="shopping"
+          bgColor="secondary"
+          title={(
+            <Typography>
+              <Box component="span" fontWeight={600}>
+                {t('modals.settings.productKey.messageBox.title')}
+              </Box>
+            </Typography>
+          )}
+        >
+          <Box mb="11px">
+            <Typography>
+              {t('modals.settings.productKey.messageBox.description')}
+            </Typography>
+          </Box>
+          {/* TODO: change to real space billing url */}
+          <Button variant="contained" onClick={() => shell.openExternal('https://space.storage')}>
+            {t('modals.settings.productKey.messageBox.buy')}
+          </Button>
+        </MessageBox>
+      </Box>
+    </>
   );
 };
 
---
+++ src/shared/components/Modal/Settings/views/ProductKey/styles.js
@@ -0,0 +1,27 @@
+import { makeStyles } from '@material-ui/core/styles';
+
+export default makeStyles((theme) => ({
+  colorBorder: {
+    borderRadius: 6,
+    padding: 4,
+    // eslint-disable-next-line max-len
+    background: 'linear-gradient(134deg, #ed55eb 18%, #17e0d8 42%, #00ffc2 59%, #ffec06 81%)',
+    marginBottom: 11,
+  },
+  contentWrapper: {
+    display: 'flex',
+    flexDirection: 'column',
+    padding: '25px 40px',
+    borderRadius: 4,
+    background: theme.palette.palette.white,
+    textAlign: 'center',
+  },
+  title: {
+    fontWeight: 600,
+    fontSize: 22,
+    margin: '16px 0 7px',
+  },
+  logo: {
+    height: 41,
+  },
+}));
