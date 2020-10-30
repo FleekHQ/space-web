b44c243fdedd992e7cfe38386889055c6e8cad7c
Giancarlo Otarola <g.otarolarobledo@gmail.com>
2020-10-30 16:52:11 -0300

    add files

---
+++ src/views/Auth/ForgotPassword/styles.js
@@ -0,0 +1,12 @@
+import { makeStyles } from '@material-ui/styles';
+
+export default makeStyles({
+  options: {
+    '& > div': {
+      marginBottom: 14,
+    },
+  },
+  keyIcon: {
+    fontSize: 17,
+  },
+});
