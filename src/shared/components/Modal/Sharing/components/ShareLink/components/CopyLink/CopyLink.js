763f4bceca36235f1980f16e98529a23faf90107
sagostinelli <batonenbois@gmail.com>
2020-11-10 11:16:47 -0500

    added clear timer

--- src/shared/components/Modal/Sharing/components/ShareLink/components/CopyLink/CopyLink.js
+++ src/shared/components/Modal/Sharing/components/ShareLink/components/CopyLink/CopyLink.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
 import copy from 'copy-to-clipboard';
 import { useTranslation } from 'react-i18next';
@@ -15,15 +15,26 @@ const CopyLink = ({ url }) => {
   const classes = useStyles();
   const { t } = useTranslation();
   const [showChecked, setShowChecked] = useState(false);
+  const [timerId, setTimerId] = useState(null);
 
   const onClick = () => {
     copy(url);
     setShowChecked(true);
 
-    setTimeout(() => {
+    clearTimeout(timerId);
+    const timer = setTimeout(() => {
       setShowChecked(false);
+      setTimerId(null);
     }, 3000);
+
+    setTimerId(timer);
   };
+
+  useEffect(() => () => {
+    if (timerId) {
+      clearTimeout(timerId);
+    }
+  }, []);
 
   return (
     <div className={classes.root}>
