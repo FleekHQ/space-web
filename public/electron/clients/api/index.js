46ee61364f03a50acd7a1d8ae184738586b03d8f
Giancarlo Otarola <g.otarolarobledo@gmail.com>
2020-11-12 16:18:57 -0300

    move some config fields to env vars

--- public/electron/clients/api/index.js
+++ public/electron/clients/api/index.js
@@ -1,11 +1,11 @@
 const ApiClient = require('./client');
 
 // eslint-disable-next-line no-console
-console.log(`Init ApiClient pointing to: ${process.env.SPACE_SERVICES_URL}`);
+console.log(`Init ApiClient, URL: ${process.env.SPACE_SERVICES_URL}`);
 
 const apiClient = new ApiClient({
   timeout: 5000,
-  baseURL: process.env.SPACE_SERVICES_URL || 'https://api-dev.space.storage',
+  baseURL: process.env.SPACE_SERVICES_URL,
 });
 
 module.exports = apiClient;
--- src/config.js
+++ src/config.js
@@ -1,16 +1,25 @@
-const { REACT_APP_FE_NODE_ENV } = process.env;
+const {
+  REACT_APP_FE_NODE_ENV,
+  TORUS_GOOGLE_VERIFIER = 'space-google-testnet',
+  TORUS_TWITTER_VERIFIER = 'fleek-auth0-twitter',
+  TORUS_TWITTER_CLIENT_ID = 'jMfyGURX9wbS3GpusgLOZkvOn0AvIaJj',
+  TORUS_TWITTER_AUTH_DOMAIN = 'https://space-twitter.us.auth0.com',
+  TORUS_PROVIDERS_REDIRECT_URL = 'https://floral-mountain-2401.on.fleek.co/',
+  WS_AUTH_CHALLENGE_URL = 'wss://gqo1oqz055.execute-api.us-west-2.amazonaws.com/dev',
+  TORUS_GOOGLE_CLIENT_ID = '480855465278-s0lcvq0m4hpn5hokhgs8s1ujfe3lbk4f.apps.googleusercontent.com',
+} = process.env;
 
 const config = {
   development: {
     ws: {
-      url: 'wss://gqo1oqz055.execute-api.us-west-2.amazonaws.com/dev',
+      url: WS_AUTH_CHALLENGE_URL,
     },
     torus: {
       sdkConfig: {
-        enableLogging: true,
+        enableLogging: REACT_APP_FE_NODE_ENV !== 'production',
         redirectToOpener: true,
         redirectPathName: 'redirect.html',
-        baseUrl: 'https://floral-mountain-2401.on.fleek.co/',
+        baseUrl: TORUS_PROVIDERS_REDIRECT_URL,
         proxyContractAddress: '0x4023d2a0D330bF11426B12C6144Cfb96B7fa6183', // details for test net
         network: 'testnet', // details for test net
       },
@@ -18,17 +27,17 @@ const config = {
         google: {
           name: 'Google',
           typeOfLogin: 'google',
-          clientId: '480855465278-s0lcvq0m4hpn5hokhgs8s1ujfe3lbk4f.apps.googleusercontent.com',
-          verifier: 'space-google-testnet',
           jwtParams: {},
+          verifier: TORUS_GOOGLE_VERIFIER,
+          clientId: TORUS_GOOGLE_CLIENT_ID,
         },
         twitter: {
           name: 'Twitter',
           typeOfLogin: 'twitter',
-          clientId: 'jMfyGURX9wbS3GpusgLOZkvOn0AvIaJj',
-          verifier: 'fleek-auth0-twitter',
+          verifier: TORUS_TWITTER_VERIFIER,
+          clientId: TORUS_TWITTER_CLIENT_ID,
           jwtParams: {
-            domain: 'https://space-twitter.us.auth0.com',
+            domain: TORUS_TWITTER_AUTH_DOMAIN,
           },
         },
       },
