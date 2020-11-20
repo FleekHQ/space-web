9b70a2d2069b3c5b3d86b2db43a9dde6e62aecc8
Giancarlo Otarola <g.otarolarobledo@gmail.com>
2020-11-20 18:03:02 -0300

    cleanup

--- src/store.js
+++ src/store.js
@@ -9,20 +9,6 @@ const composeEnhancers = typeof window === 'object' && window.__REDUX_DEVTOOLS_E
 
 const enhancer = composeEnhancers(applyMiddleware(thunk));
 
-const store = createStore(rootReducer, {
-  // TODO: remove hardcoded init state
-  user: {
-    uuid: '1f43cec7-472b-47d8-b4ec-08d1d8f5e687',
-    address: '0x9fa66f2cc560cbeaf85cb6ce6756ba77f655',
-    publicKey: '4a0d2893c9e57839195acb5dfb818c9782f89136ff6d5a4dcea9626c3d74d502',
-    username: 'teester20m',
-    avatarUrl: '',
-  },
-  welcome: {
-    hideBackup: false,
-    hideUsername: false,
-    hideIntegration: false,
-  },
-}, enhancer);
+const store = createStore(rootReducer, {}, enhancer);
 
 export default store;
