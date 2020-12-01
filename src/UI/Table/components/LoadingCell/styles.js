240b8e25e5c491d6f3dedf1386ca122e652d0f10
Guillermo Puente <guillermo.ps09@gmail.com>
2020-12-01 16:36:51 -0300

    updated skeleton

--- src/UI/Table/components/LoadingCell/styles.js
+++ src/UI/Table/components/LoadingCell/styles.js
@@ -1,16 +1,10 @@
 import { makeStyles } from '@material-ui/core/styles';
 
 export default makeStyles({
-  iconCellContainer: {
-    display: 'flex',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  iconCell: {
-    marginRight: 6,
-    display: 'inline-block',
-  },
   cell: {
+    borderRadius: 4,
     display: 'inline-block',
+    width: '100%',
+    backgroundColor: '#EBEEF3',
   },
 });
