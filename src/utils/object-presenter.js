dcee5bda184d9fce6e53c2a178a42a7de0171ba2
sagostinelli <batonenbois@gmail.com>
2020-12-04 18:14:41 -0500

    remove isUploading

--- src/utils/object-presenter.js
+++ src/utils/object-presenter.js
@@ -46,7 +46,6 @@ const objectPresenter = (obj = {}, isRootDir = false) => {
     isAvailableInSpace: backupCount > 0,
     sourceBucket: sourceBucket || bucket,
     shareAmount: Math.max(1, members.length),
-    isUploading: true,
   };
 };
 
