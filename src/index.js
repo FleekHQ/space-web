8c1a17ff2d2ffc9ef9f55710368f4c9a800ac96e
Giancarlo Otarola <g.otarolarobledo@gmail.com>
2021-01-20 16:32:59 -0300

    add txlSubscribe and refactor sdk init

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import i18n from 'i18next';
 import { initReactI18next } from 'react-i18next';
+
 import App from './App';
 import initConfig from './locales';
 
