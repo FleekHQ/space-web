d4df89e9f1c1ec922d40e55235f5f43f38954723
Giancarlo Otarola <g.otarolarobledo@gmail.com>
2021-01-21 17:23:43 -0300

    init changes

--- src/shared/components/Modal/Settings/views/index.js
+++ src/shared/components/Modal/Settings/views/index.js
@@ -1,4 +1,3 @@
 export { default as Account } from './Account';
-export { default as Security } from './Security';
 export { default as Usage } from './Usage';
 export { default as ProductKey } from './ProductKey';
