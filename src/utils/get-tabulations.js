bf1af93524799ccb7c7b42fe291fd37143a58685
Guillermo Puente <guillermo.ps09@gmail.com>
2021-01-25 06:39:52 -0300

    Fix folder selection inside subfolder

--- src/utils/get-tabulations.js
+++ src/utils/get-tabulations.js
@@ -1,6 +1,6 @@
 const getTabulations = (rowKey, location) => {
   const locationWithRoot = location.pathname.split('/').filter((folder) => folder !== '');
-  const locationWithoutRoot = locationWithRoot.slice(2, locationWithRoot.length);
+  const locationWithoutRoot = locationWithRoot.slice(1, locationWithRoot.length);
   const rootFolderAmount = locationWithoutRoot.length;
   const currentItemFolderAmount = rowKey.split('/').length;
   const tabulations = currentItemFolderAmount - rootFolderAmount - 1;
