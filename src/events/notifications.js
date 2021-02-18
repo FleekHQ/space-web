9d8e31f8b5f274dd8ddbd0893185f2d6e08a34cf
sagostinelli <batonenbois@gmail.com>
2021-02-18 17:56:26 -0500

    handleinvitation (not testd)

--- src/events/notifications.js
+++ src/events/notifications.js
@@ -2,6 +2,9 @@
 /* eslint-disable no-unused-vars */
 
 import { sdk } from '@clients';
+import { ERROR_MODAL_TOAST, OPEN_MODAL } from '@shared/components/Modal/actions';
+import { fetchSharedObjects } from './objects';
+
 import store from '../store';
 import { NOTIFICATIONS_ACTION_TYPES } from '../reducers/notifications';
 
@@ -23,12 +26,57 @@ export const fetchNotifications = async (payload) => {
   });
 };
 
-export const handleFilesInvitation = (payload) => {
+export const handleFilesInvitation = async (payload) => {
   store.dispatch({
     type: NOTIFICATIONS_ACTION_TYPES.ON_UPDATE_INVITATION_STATUS,
     status: payload.accept ? 'ACCEPTING' : 'REJECTING',
     ...payload,
   });
+
+  try {
+    const storage = await sdk.getStorage();
+    await storage.acceptFileInvitation({
+      invitation: {
+        invitationID: payload.invitationID,
+        status: payload.accept ? 1 : 2,
+        // inviteePublicKey: '',
+        // inviterPublicKey: '',
+        // itemPaths: [],
+        // keys: [],
+      },
+    });
+    fetchSharedObjects();
+
+    store.dispatch({
+      type: NOTIFICATIONS_ACTION_TYPES.ON_UPDATE_INVITATION_STATUS,
+      status: payload.accept ? 'ACCEPTED' : 'REJECTED',
+      ...payload,
+    });
+
+    if (payload.accept) {
+      payload.history.push('/shared');
+    }
+  } catch (e) {
+    store.dispatch({
+      type: NOTIFICATIONS_ACTION_TYPES.ON_UPDATE_INVITATION_STATUS,
+      status: 'PENDING',
+      ...payload,
+    });
+
+    const props = {
+      i18nKey: 'errorModal.handleInvitationFailed',
+    };
+
+    store.dispatch({
+      type: OPEN_MODAL,
+      payload: {
+        id: 'handle-invitation-failed',
+        type: ERROR_MODAL_TOAST,
+        props,
+      },
+    });
+    console.error('Failed to update invitation status', e);
+  }
 };
 
 export const setNotificationsLastSeenAt = (payload) => {
--- src/shared/components/Notifications/Notifications.js
+++ src/shared/components/Notifications/Notifications.js
@@ -11,6 +11,7 @@ import {
   NotificationMenu,
   NotificationButton,
 } from '@ui/Notification';
+import { useHistory } from 'react-router-dom';
 import { useDispatch, useSelector } from 'react-redux';
 import { openModal, LICENSE_REGISTRATION } from '@shared/components/Modal/actions';
 import mapDataToItems from './utils/map-data-to-items';
@@ -21,6 +22,7 @@ const Notifications = () => {
   const classes = useStyles();
   const dispatch = useDispatch();
   const { t } = useTranslation();
+  const history = useHistory();
 
   const [highlighted, setHighlighted] = useState(false);
   const [anchorEl, setAnchorEl] = useState(null);
@@ -109,6 +111,7 @@ const Notifications = () => {
         id: item.id,
         invitationID: foundNotification.invitationValue.invitationID,
         accept,
+        history,
       });
     }
   };
